29f6ff7bdb44ae2bb74c1744d79c7a6ca389d990
JJChmielewski <j.j.chmielewski@wp.pl>
2022-03-22 19:38:07 +0100

    Working on connecting service to controller

--- src/main/java/com/boil/cpm/controller/NodeController.java
+++ src/main/java/com/boil/cpm/controller/NodeController.java
@@ -3,6 +3,7 @@ package com.boil.cpm.controller;
 
 import com.boil.cpm.entities.Action;
 import com.boil.cpm.entities.Node;
+import com.boil.cpm.service.NodeService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,7 +11,9 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +24,12 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class NodeController {
+    private final NodeService nodeService;
+
+    @Autowired
+    public NodeController(NodeService nodeService) {
+        this.nodeService = nodeService;
+    }
 
     @GetMapping("/")
     public String nodeListGet() {
@@ -29,7 +38,7 @@ public class NodeController {
     }
 
     @PostMapping("/calculate")
-    public String nodeListPost(@RequestBody MultiValueMap<String, String> values, BindingResult result) {
+    public String nodeListPost(@RequestBody MultiValueMap<String, String> values, BindingResult result, Model model) {
         if(result.hasErrors()) {
             return "error";
         }
@@ -46,6 +55,12 @@ public class NodeController {
         }
 
         // TODO OBSŁUGA NODÓW
+
+        Node root = nodeService.buildNetwork((Action[]) actions.toArray());
+
+        model.addAttribute("rootNode", root);
+
+        System.out.println(root);
 
         return "nodes_cpm";
     }
